8f885eaa7709d48f767b355cb63deb0ba1ca28c8
Dmitry Pershin <planeta977@mail.ru>
2021-03-21 09:20:18 +0300

    add template authorization

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('Hellp World');
+import tml from './templates/index.tmp'
+const loginPage = document.getElementsByClassName('login-page')[0];
+
+const objData = {
+    input: 'Вход',
+    authorization: 'Авторизоваться',
+    noAccount: 'Нет аккаунта?'
+}
+
+let template = Handlebars.compile(tml);
+let html = template(objData);
+let content = new DOMParser().parseFromString(html, "text/html").body.childNodes[0];
+loginPage.appendChild(content);
---
+++ src/templates/index.tmp.js
@@ -0,0 +1,13 @@
+let tml = `<div class="form-authorization">
+                <div class="form-authorization__head">{{input}}</div>
+                <input type="text" placeholder="Логин" class="form-authorization__loginInput">
+                <input type="password" placeholder="Пароль" class="form-authorization__passInput">
+                <div class="form-authorization__button">
+                    <span>Авторизоваться</span>
+                </div>
+                <div class="form-authorization__reg">
+                    <span>Нет аккаунта?</span>
+                </div>
+            </div>`;
+
+module.exports = tml;
